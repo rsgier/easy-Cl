9d8da932bed36364e1cd5c91b86ed14878409075
Raphael Sgier <rsgier@phys.ethz.ch>
2018-09-07 09:23:23 +0200

    changed parameters of functions in ecl.py and kernel_matrix.py

--- ECl/ecl.py
+++ ECl/ecl.py
@@ -9,6 +9,8 @@ from __future__ import (print_function, division, absolute_import,
 
 #import ECl
 import numpy as np
+import healpy as hp
+import time
 from ECl.kernel_matrix import KernelMatrix
 
 
@@ -22,7 +24,7 @@ class PseudoCl(object):
     def __init__(self):
         pass
 
-    def pseudocl(self, weight_map, cl_theory, nside=1024):
+    def pseudocl(self, cl_theory, weight_map=True, l_max = 200, nside=1024):
         """
         :param weight_map: map containing the weights to compute the power spectrum of the weights.
         :param cl_theory: theoretical prediction for the full-sky EE angular shear power spectrum.
@@ -30,15 +32,19 @@ class PseudoCl(object):
         :return: pseudo power spectrum computed using the kernel matrix based on the weight map.
         """
 
-        l_max = 50
-        print('calculating power spectrum of the weight_map...')
-        maskps = KM.maskpowerspectrum(weight_map, nside)
-        print('done!')
+        if weight_map is True:
+            print('calculating power spectrum of the weight_map...')
+            maskps = np.array(hp.sphtfunc.anafast((weight_map)))
+            print('done!')
+        else:
+            print('no weight map is used.')
+            maskps = np.array(hp.sphtfunc.anafast((np.ones(hp.nside2npix(nside)))))
         print('calculating kernel matrix...')
+        start = time.time()
         M = KM.kernelmatrix(maskps, l_max, l_max)
+        end = time.time()
         print('done!')
+        print('time elapsed to compute kernel matrix: {} s.'.format(end - start))
         return M.dot(cl_theory[0:l_max])
-
-
 
 
