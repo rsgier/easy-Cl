b730960b4863385157754383b17a6347103252c2
Jörg Herbel <joerg.herbel@phys.ethz.ch>
2019-04-05 14:24:36 +0200

    adjusted __init__

--- ECl/__init__.py
+++ ECl/__init__.py
@@ -1,4 +1,7 @@
+import pkg_resources
+from . import catalog_to_map, mode_coupling_matrix, run_anafast, run_polspice, utils
+
 __author__ = 'Raphael Sgier'
 __email__ = 'rsgier@phys.ethz.ch'
-__version__ = '0.1.0'
 __credits__ = 'ETH Zurich, Institute for Particle Physics and Astrophysics'
+__version__ = pkg_resources.require(__package__)[0].version
