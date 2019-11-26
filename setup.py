d492cb1ec3ccdf9547f4f08b75cb88975a6e8e10
Jörg Herbel <joerg.herbel@phys.ethz.ch>
2019-11-26 18:38:02 +0100

    updated setup.py, requirements_dev.txt, tox.ini and .gitlab-ci.yml

--- setup.py
+++ setup.py
@@ -1,31 +1,22 @@
 #!/usr/bin/env python
 
 import os
-import sys
-from setuptools.command.test import test as TestCommand
-from setuptools import find_packages
-from setuptools import setup
+
+from setuptools import find_packages, setup
 
 
-class PyTest(TestCommand):
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        import pytest
-        errno = pytest.main(self.test_args)
-        sys.exit(errno)
+requirements = ['numpy',
+                'healpy',
+                'numba',
+                'pyshtools']  # during runtime
 
 
-try:
-    readme = open('README.rst').read()
-except IOError:
-    # when this package is installed as dependency some files might not be
-    # present:
-    readme = ""
+with open("README.rst") as readme_file:
+    readme = readme_file.read()
+
+with open("HISTORY.rst") as history_file:
+    history = history_file.read().replace(".. :changelog", "")
+
 
 doclink = """
 Documentation
@@ -33,19 +24,9 @@ Documentation
 
 The full documentation can be generated with Sphinx"""
 
-try:
-    history = open('HISTORY.rst').read().replace('.. :changelog:', '')
-    # when this package is installed as dependency some files might not be
-    # present:
-except IOError:
-    history = ""
-
-requires = ['numpy',
-            'healpy',
-            'numba',
-            'pyshtools']
 
 PACKAGE_PATH = os.path.abspath(os.path.join(__file__, os.pardir))
+
 
 setup(
     name='ECl',
@@ -55,21 +36,18 @@ setup(
     author='Raphael Sgier',
     author_email='rsgier@phys.ethz.ch',
     url='https://cosmo-docs.phys.ethz.ch/ECl',
-    packages=find_packages(PACKAGE_PATH, "test"),
-    package_dir={'ECl': 'ECl'},
+    packages=find_packages(include=["ECl"]),
     include_package_data=True,
-    install_requires=requires,
-    license='Proprietary',
+    install_requires=requirements,
+    license="Proprietary",
     zip_safe=False,
-    keywords='ECl',
+    keywords="ECl",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Science/Research",
-        'Intended Audience :: Developers',
-        'License :: Other/Proprietary License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.6",
     ],
-    cmdclass={'test': PyTest},
 )
